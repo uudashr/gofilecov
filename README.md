17016bda2b46695b7bf00bf496c02c9a8b5a5c69
Nuruddin Ashr <uudashr@gmail.com>
2019-02-21 15:39:59 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Go File Coverage
+
+This is Go tool to read coverage profile and show the coverage reports on file basis.
+
+## Why?
+On legacy app, it's is hard to add unit test for the whole repository/package. This is why `gofilecov` created, so we can cover only file that modified.
+
+## Installation
+`$ go get github.com/uudahr/gofilecov`
+
+## Usage
+```
+$ gofilecov
+Usage of ./gofilecov:
+  -cover-profile string
+    	Coverage profile
+  -format string
+    	Output format (default "{{ .FileName }}\tcoverage: {{ printf \"%.1f %%\" .coverage }}")
+```
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import "flag"
+import "fmt"
+import "golang.org/x/tools/cover"
+import "os"
+import "text/template"
+
+func main() {
+	flagCoverProfile := flag.String("cover-profile", "", "Coverage profile")
+	flagFormat := flag.String("format", "{{ .FileName }}\tcoverage: {{ printf \"%.1f %%\" .Coverage }}", "Output format")
+
+	flag.Parse()
+	if *flagCoverProfile == "" || *flagFormat == "" {
+		flag.Usage()
+		os.Exit(-1)
+	}
+
+	tpl, err := template.New("out").Parse(*flagFormat)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Invalid format:", err)
+		os.Exit(-1)
+	}
+
+	profiles, err := cover.ParseProfiles(*flagCoverProfile)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Invalid coverage profiles:", err)
+		os.Exit(-1)
+	}
+
+	for _, prof := range profiles {
+		var count float32
+		for _, b := range prof.Blocks {
+			if b.Count > 0 {
+				count++
+			}
+		}
+
+		coverage := (count / float32(len(prof.Blocks))) * float32(100)
+		fc := fileCoverage{
+			FileName: prof.FileName,
+			Coverage: coverage,
+		}
+		if err = tpl.Execute(os.Stdout, fc); err != nil {
+			fmt.Fprintln(os.Stderr, "Unable to write formatted output:", err)
+		}
+
+		fmt.Fprintln(os.Stdout)
+	}
+}
+
+type fileCoverage struct {
+	FileName string
+	Coverage float32
+}
